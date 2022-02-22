88b3bdfdf0dad437aa64f1538420fa7ce2fdc259
Rebecca Sissman <rsissman15@aol.com>
2022-02-22 16:52:53 -0500

    Click on the drink page and the ehader refreshes and updates

--- javascripts/index.js
+++ javascripts/index.js
@@ -5,14 +5,30 @@ const mainDiv=()=>document.getElementById("main");
 //Event Handler
 const renderDrinksPage=(e)=>{
     e.preventDefault();
-    alert('hi');
-}
+    resetMain();
 
+     //create header and style it
+     const h3=document.createElement('h3');
+     h3.innerHTML="Drinks List";
+     h3.className="center-align";
+     h3.style.marginTop="10px";
+     h3.style.paddingTop="10px";
+
+     mainDiv().appendChild(h3);
+     
+   
+}
 
 //Event Listener
 const attachDrinksListEvent=()=>{
     drinkList().addEventListener('click',renderDrinksPage)
 }
+
+//Resets the page
+const resetMain=()=>{
+    mainDiv().innerHTML="";
+}
+
 
 document.addEventListener('DOMContentLoaded',()=>{
     attachDrinksListEvent();
